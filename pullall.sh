3fabaab9745fce1bffad9ed3e1d24f0b08098137
Luis Amaro <lamaro@poppulo.com>
2021-11-10 10:44:39 +0000

    Add very simple git pull all script

---
+++ pullall.sh
@@ -0,0 +1,22 @@
+# simple script to pull/update all repos
+
+#! /usr/bin/env bash
+
+BLUE='\033[0;34m'
+CIAN='\033[0;36m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NOCOLOR='\033[0m'
+
+for FOLDER in $(ls -1d -- *)
+do
+  [[ -d "${FOLDER}" ]] && \
+  [[ -d "${FOLDER}/.git" ]] && \
+  echo -e "\n${CIAN}Repo: ${BLUE}${FOLDER}${NOCOLOR}" && \
+  echo -e "${CIAN}Branch: ${BLUE}$(awk -F/ '{print $3}' ${FOLDER}/.git/HEAD)${NOCOLOR}" && \
+  cd "${FOLDER}" && \
+  git pull && \
+  cd ..
+done
+
+echo -e "\n${YELLOW}Happy pulling${NOCOLOR}"
